37ed4c29c9d4ff53a6c3f5a608cab829f7f39252
clementhtn <clement.hatton77@gmail.com>
2019-02-11 12:02:12 +0100

    create function read

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,23 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
-use Symphony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController
 {
-    /*
+    /**
      * @Route("/")
      */
     public function homepage ()
     {
         return new Response( "OMG this is my first page! :D" );
+    }
+
+    /**
+     * @Route("/article/{page}")
+     */
+    public function read ()
+    {
+        return new Response( "L'article article" );
     }
 }
