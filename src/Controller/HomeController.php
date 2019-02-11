a5a2f9017beb69557bad8a74f602d56aede0769f
clementhtn <clement.hatton77@gmail.com>
2019-02-11 11:19:05 +0100

    add route in HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,11 +4,15 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symphony\Component\Routing\Annotation\Route;
 
 class HomeController
 {
-    public function homepage()
+    /*
+     * @Route("/")
+     */
+    public function homepage ()
     {
-        return new Response("OMG this is my first page! :D");
+        return new Response( "OMG this is my first page! :D" );
     }
 }
